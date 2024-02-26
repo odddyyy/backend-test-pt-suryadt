cfafb778f72fa03512c51cad8996d13cbce17314
odddyyy <oddy.sutrisno@gmail.com>
2024-02-26 20:50:47 +0700

    batching query per 500 in each loop

--- src/cron/birthday.cron.ts
+++ src/cron/birthday.cron.ts
@@ -6,35 +6,56 @@ import failedEmailLog from '../models/failed-email-log.model';
 // cron for sending email to birthday user (runs every hour)
 const birthdayJob = new cron.CronJob('0 * * * *', async () => {
   const currentDate: Date = new Date();
-  const users = await user.find({ isSentEmail: false });
-  for (const i of users) {
-    const userBirthday: Date = new Date(i.birthday);
-    if (currentDate.getMonth() == userBirthday.getMonth() && currentDate.getDate() == userBirthday.getDate()) {
-      const timezone = i.location;
-      const localTime = new Date(currentDate.toLocaleString('en-US', { timeZone: timezone }));
-      if (localTime.getHours() === 9 && localTime.getMinutes() === 0) {
-        const success = await sendBirthDayEmail(String(i._id), i.firstName, i.lastName);
-        // update the user isSentEmail to true
-        if (success) {
-          await user.updateOne({ _id: i._id }, { isSentEmail: true });
+  let lastId: any = null;
+  while (true) {
+    let users;
+    if (!lastId) {
+      users = await user.find({ isSentEmail: false }).sort({ _id: 1 }).limit(500);
+    } else {
+      users = await user.find({ _id: { $gt: lastId }, isSentEmail: false })
+    }
+    if (users.length === 0) {
+      break;
+    }
+    for (const i of users) {
+      const userBirthday: Date = new Date(i.birthday);
+      if (currentDate.getMonth() == userBirthday.getMonth() && currentDate.getDate() == userBirthday.getDate()) {
+        const timezone = i.location;
+        const localTime = new Date(currentDate.toLocaleString('en-US', { timeZone: timezone }));
+        if (localTime.getHours() === 9 && localTime.getMinutes() === 0) {
+          const success = await sendBirthDayEmail(String(i._id), i.firstName, i.lastName);
+          // update the user isSentEmail to true
+          if (success) {
+            await user.updateOne({ _id: i._id }, { isSentEmail: true });
+          }
         }
       }
     }
+    lastId = users[users.length - 1]._id;
   }
 });
 
 // cron for retrying failed email sent (runs every hour)
 const failedRetry = new cron.CronJob('0 * * * *', async () => {
   const currentDate: Date = new Date();
-  const failedEmail = await failedEmailLog.find({ time: { $lte: currentDate } });
-  for (const i of failedEmail) {
-    const data = await user.findById(i.userId);
-    const success = await sendBirthDayEmail(String(data._id), data.firstName, data.lastName);
-    if (success) {
-      // if succes update the isSentEmail and delete the failedEmailLog
-      await user.updateOne({ _id: data._id }, { isSentEmail: true });
-      await failedEmailLog.deleteOne({ _id: i._id });
+  let lastId: any = null
+  while(true) {
+    let failedEmail;
+    if (!lastId) {
+      failedEmail = await failedEmailLog.find({ time: { $lte: currentDate } }).limit(500).sort({ _id: 1 });
+    } else {
+      failedEmail = await failedEmailLog.find({ _id: { $gt: lastId }, time: { $lte: currentDate }});
     }
+    for (const i of failedEmail) {
+      const data = await user.findById(i.userId);
+      const success = await sendBirthDayEmail(String(data._id), data.firstName, data.lastName);
+      if (success) {
+        // if succes update the isSentEmail and delete the failedEmailLog
+        await user.updateOne({ _id: data._id }, { isSentEmail: true });
+        await failedEmailLog.deleteOne({ _id: i._id });
+      }
+    }
+    lastId = failedEmail[failedEmail.length - 1]._id;
   }
 });
 
